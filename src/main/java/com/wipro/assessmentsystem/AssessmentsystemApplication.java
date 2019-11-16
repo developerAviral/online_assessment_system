7926d355cc781fdbd4f1ab675bfc0d6fb8d11cc4
Aviral_Srivastava <developer.aviral@gmail.com>
2019-11-16 20:31:07 +0530

    initial commit

---
+++ src/main/java/com/wipro/assessmentsystem/AssessmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.assessmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wipro/assessmentsystem/AssessmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.assessmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
